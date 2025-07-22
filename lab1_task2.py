e3717fa80e4b060a7b2706a60dc17d3529574230
mafimashrafi <mafimashrafi78@gmail.com>
2025-07-23 03:20:19 +0600

    blinking left

--- lab1_task2.py
+++ lab1_task2.py
@@ -4,40 +4,102 @@ from OpenGL.GLU import *
 import random
 
 w_width, w_height = 500, 500
+speed = 0.1
+vertex_x, vertex_y = random.randint(0, w_width), random.randint(0, 500)
+directions = [[-1, 1], [1, 1], [-1, -1], [1, -1]]
+direction = random.choice(directions)
+coordinates = []
+R, G, B = random.random(), random.random(), random.random()
+coordinates.append([vertex_x, vertex_y, direction[0], direction[1], (R, G, B)])
+pause = True
 
 
 class createDot:
-    def draw(self):
-        global w_width, w_height 
-
-        R = random.random()
-        G = random.random()
-        B = random.random()
-
-        vertex_x = random.randint(0, 500)
-        vertex_y = random.randint(0, 500)
+    def draw(self, vertex_x, vertex_y, color):
 
         glPointSize(5)
         glBegin(GL_POINTS)
-        glColor3f(R, G, B)
+        glColor3f(*color)
         glVertex2f(vertex_x, vertex_y)
         glEnd()
 
+
 def display():
+    global w_width, w_height, vertex_x, vertex_y, coordinates
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glMatrixMode(GL_MODELVIEW)
 
-    createDot().draw()
+    for x, y, _, _, color in coordinates:
+        createDot().draw(x, y, color)
 
     glutSwapBuffers()
 
+def mouse_listener(button, state, x, y):
+    global w_width, w_height, coordinates, directions
+    if button == GLUT_RIGHT_BUTTON:
+        if state == GLUT_DOWN:
+            vertex_x = random.randint(0, w_width)
+            vertex_y = random.randint(0, w_height)
+            direction = random.choice(directions)
+            R, G, B = random.random(), random.random(), random.random()
+            coordinates.append([vertex_x, vertex_y, direction[0], direction[1], (R, G, B)]) 
+            glutPostRedisplay()
+
+def specialkey_listener(key, x, y):
+    global speed
+    if key == GLUT_KEY_UP:
+        speed+=0.1
+    if key == GLUT_KEY_DOWN:
+        speed-=0.1
+        if speed < 0 :
+            speed = 0.01
+
+    glutPostRedisplay()
+
+def normal_key_listener(key, x, y):
+    global pause
+    if key == b' ':
+        pause = not pause
+
+    glutPostRedisplay()
+    
+
+def animate():
+    global w_width, w_height, coordinates
+    if pause:
+        for dots in coordinates:
+            vertex_x, vertex_y, direction_x, direction_y, color = dots
+            if vertex_x >= w_width:
+                direction_x *= (-1)
+            elif vertex_x < 0:
+                direction_x *= -1
+                
+            if vertex_y >= w_height:
+                direction_y *= (-1)
+            elif vertex_y <= 0:
+                direction_y *= -1
+            
+            vertex_x += speed*direction_x
+            vertex_y += speed*direction_y
+
+            dots[0] = vertex_x
+            dots[1] = vertex_y
+            dots[2] = direction_x
+            dots[3] = direction_y
+            dots[4] = color
+
+    glutPostRedisplay()
+
+
 def init():
     glColor3f(1, 1, 1)
-    glMatrixMode(GL_MODELVIEW)
+    glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(104, 1, 1, 1000)
+    gluOrtho2D(0, w_width, 0, w_height)
+    glMatrixMode(GL_MODELVIEW)
 
 glutInit()
 glutInitWindowSize(w_width, w_height)
@@ -48,6 +110,9 @@ window = glutCreateWindow(b'Magic Box')
 init()
 
 glutDisplayFunc(display)
-
+glutIdleFunc(animate)
+glutMouseFunc(mouse_listener)
+glutSpecialFunc(specialkey_listener)
+glutKeyboardFunc(normal_key_listener)
 
 glutMainLoop()
