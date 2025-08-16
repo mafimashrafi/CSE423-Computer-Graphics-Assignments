660e46905814acde281c62cdb5846d4feca24a9c
mafimashrafi <mafimashrafi78@gmail.com>
2025-08-16 23:17:38 +0600

    no change just looking around

--- 3D_OpenGL_Intro.py
+++ 3D_OpenGL_Intro.py
@@ -48,10 +48,10 @@ def draw_shapes():
 
     glColor3f(1, 1, 0)
     glScalef(2, 2, 2)
-    gluCylinder(gluNewQuadric(), 40, 5, 150, 100, 100)  # parameters are: quadric, base radius, top radius, height, slices, stacks
+    gluCylinder(gluNewQuadric(), 40, 5, 150, 10, 10)  # parameters are: quadric, base radius, top radius, height, slices, stacks
     glTranslatef(100, 0, 100) 
     glRotatef(90, 0, 1, 0)  # parameters are: angle, x, y, z
-    gluCylinder(gluNewQuadric(), 40, 5, 150, 144, 144)
+    gluCylinder(gluNewQuadric(), 40, 5, 150, 10, 10)
 
     glColor3f(0, 1, 1)
     glTranslatef(300, 0, 100) 
