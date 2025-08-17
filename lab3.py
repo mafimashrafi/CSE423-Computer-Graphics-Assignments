0f3194a4eed0a70cdd3b1313d304879c4f75a005
mafimashrafi <mafimashrafi78@gmail.com>
2025-08-17 23:35:49 +0600

    initial camera setup

--- lab3.py
+++ lab3.py
@@ -3,11 +3,27 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 Grid_lentgh = 600
+camera_pos = (0, 500, 500)
+fovY = 120 
+
+def camera_setup():
+    global camera_pos, fovY
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(fovY, 1000/800, 0.1, 1000) 
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    x, y, z = camera_pos
+    gluLookAt(x, y, z, 
+              0, 0, 0, 
+              0, 1, 0)
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     glViewport(0, 0, 1000, 800)
+    camera_setup()
     glutSwapBuffers()
 
 def init():
