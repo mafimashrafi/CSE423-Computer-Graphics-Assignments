1fe897a8472e142674f2c3990195a6732e03d802
mafimashrafi <mafimashrafi78@gmail.com>
2025-08-18 00:27:46 +0600

    trial 2 wiht the help of AI

--- lab3.py
+++ lab3.py
@@ -2,78 +2,70 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-Grid_lentgh = 60
-camera_pos = (0, 500, 500)
-fovY = 120 
+Grid_length = 60
+camera_pos = (0, 400, 600)  # camera placed diagonally above the grid
+fovY = 120
 
-def draw_grid():
-    global Grid_lentgh
+def draw_tile(is_white=True):
+    """Draw a single square tile on XY plane"""
+    if is_white:
+        glColor3f(1, 1, 1)
+    else:
+        glColor3f(0.7, 0.5, 0.95)
+
     glBegin(GL_QUADS)
-    glColor3f(1, 1, 1)
-    glVertex3f(-Grid_lentgh, Grid_lentgh, 0)
-    glVertex3f(0, Grid_lentgh, 0)
     glVertex3f(0, 0, 0)
-    glVertex3f(-Grid_lentgh, 0, 0)
-
-    glVertex3f(Grid_lentgh, -Grid_lentgh, 0)
-    glVertex3f(0, -Grid_lentgh, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(Grid_lentgh, 0, 0)
-
-
-    glColor3f(0.7, 0.5, 0.95)
-    glVertex3f(-Grid_lentgh, -Grid_lentgh, 0)
-    glVertex3f(-Grid_lentgh, 0, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, -Grid_lentgh, 0)
-
-    glVertex3f(Grid_lentgh, Grid_lentgh, 0)
-    glVertex3f(Grid_lentgh, 0, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, Grid_lentgh, 0)
+    glVertex3f(Grid_length, 0, 0)
+    glVertex3f(Grid_length, Grid_length, 0)
+    glVertex3f(0, Grid_length, 0)
     glEnd()
 
 
 def camera_setup():
-    global camera_pos, fovY, Grid_lentgh
+    global camera_pos, fovY
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(fovY, 1000/800, 0.1, 1000) 
+    gluPerspective(fovY, 1000/800, 1, 5000) 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
     x, y, z = camera_pos
-    gluLookAt(x, y, z, 
-              0, 0, 0, 
-              0, 1, 0)
+    # Camera looks at origin, Z is up
+    gluLookAt(x, y, z,
+               0, 0, 0,
+               0, 0, 1)
+
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
     glViewport(0, 0, 1000, 800)
     camera_setup()
 
-    for i in range(10):
-        for j in range(10):
+    # Draw a big checkerboard grid
+    for i in range(-10, 10):
+        for j in range(-10, 10):
             glPushMatrix()
-            glTranslatef(i * Grid_lentgh, j * Grid_lentgh, 0)
-            draw_grid()
+            glTranslatef(i * Grid_length, j * Grid_length, 0)
+
+            # alternate color
+            is_white = (i + j) % 2 == 0
+            draw_tile(is_white)
+
             glPopMatrix()
+
     glutSwapBuffers()
+
 
 def init():
     glEnable(GL_DEPTH_TEST)
-    glColor3f(1.0, 1.0, 1.0)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45, 1000/800, 0.1, 1000) 
-    glMatrixMode(GL_MODELVIEW)
+    glClearColor(0, 0, 0, 1)
+
 
 glutInit()
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
 glutInitWindowSize(1000, 800)   
 glutInitWindowPosition(100, 100)
-window = glutCreateWindow(b"3D OpenGL Intro")
+window = glutCreateWindow(b"3D OpenGL Checkerboard Grid")
 init()
 glutDisplayFunc(display)
 
