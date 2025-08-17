1e39e2e5f7030256f6ea5258db5829fde37ff0cf
mafimashrafi <mafimashrafi78@gmail.com>
2025-08-18 00:06:11 +0600

    trying to make the grid: First trial failed

--- lab3.py
+++ lab3.py
@@ -2,12 +2,40 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-Grid_lentgh = 600
+Grid_lentgh = 60
 camera_pos = (0, 500, 500)
 fovY = 120 
 
+def draw_grid():
+    global Grid_lentgh
+    glBegin(GL_QUADS)
+    glColor3f(1, 1, 1)
+    glVertex3f(-Grid_lentgh, Grid_lentgh, 0)
+    glVertex3f(0, Grid_lentgh, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(-Grid_lentgh, 0, 0)
+
+    glVertex3f(Grid_lentgh, -Grid_lentgh, 0)
+    glVertex3f(0, -Grid_lentgh, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(Grid_lentgh, 0, 0)
+
+
+    glColor3f(0.7, 0.5, 0.95)
+    glVertex3f(-Grid_lentgh, -Grid_lentgh, 0)
+    glVertex3f(-Grid_lentgh, 0, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, -Grid_lentgh, 0)
+
+    glVertex3f(Grid_lentgh, Grid_lentgh, 0)
+    glVertex3f(Grid_lentgh, 0, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, Grid_lentgh, 0)
+    glEnd()
+
+
 def camera_setup():
-    global camera_pos, fovY
+    global camera_pos, fovY, Grid_lentgh
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(fovY, 1000/800, 0.1, 1000) 
@@ -24,6 +52,13 @@ def display():
     glLoadIdentity()
     glViewport(0, 0, 1000, 800)
     camera_setup()
+
+    for i in range(10):
+        for j in range(10):
+            glPushMatrix()
+            glTranslatef(i * Grid_lentgh, j * Grid_lentgh, 0)
+            draw_grid()
+            glPopMatrix()
     glutSwapBuffers()
 
 def init():
