24a0a71aa8f7adad17859d85eba1ee3592cae383
mafimashrafi <mafimashrafi78@gmail.com>
2025-08-20 00:20:20 +0600

    body work remaining guns and hands

--- lab3.py
+++ lab3.py
@@ -3,53 +3,57 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 Grid_length = 60
-camera_pos = (0, 400, 500) 
+camera_pos = (0, 400, 6) 
 fovY = 120
+player_x, player_y, player_z = 0, 0, 0 
 
 def borders_of_grid():
+    global player_x, player_y, player_z
+
     glBegin(GL_QUADS)
-    # glColor3f(0, 0.3, 1)
-    # glVertex3f(-700, -600, 50)
-    # glVertex3f(-600, -600, 50)
-    # glVertex3f(-600, 600, 50)
-    # glVertex3f(-700, 600, 50)
-
-    # glColor3f(1, 0, 0)
-    # glVertex3f(-700, -600, 0)
-    # glVertex3f(-600, -600, 0)
-    # glVertex3f(-600, 600, 0)
-    # glVertex3f(-700, 600, 0)
-
-    # glColor3f(0, 1, 0)
-    # glVertex3f(-700, -600, 50)
-    # glVertex3f(-700, -600, 0)
-    # glVertex3f(-700, 600, 0)
-    # glVertex3f(-700, 600, 50)
-
     glColor3f(0, 0, 1)
-    glVertex3f(-600, -600, 50)
+    glVertex3f(-600, -600, 100)
     glVertex3f(-600, -600, 0)
     glVertex3f(-600, 600, 0)
-    glVertex3f(-600, 600, 50)
+    glVertex3f(-600, 600, 100)
 
     glColor3f(1, 0, 0)
-    glVertex3f(-600, 600, 50)
+    glVertex3f(-600, 600, 100)
     glVertex3f(-600, 600, 0)
     glVertex3f(600, 600, 0)    
-    glVertex3f(600, 600, 50)
+    glVertex3f(600, 600, 100)
 
     glColor3f(0, 1, 0)
-    glVertex3f(600, 600, 50)    
+    glVertex3f(600, 600, 100)    
     glVertex3f(600, 600, 0)
     glVertex3f(600, -600, 0)
-    glVertex3f(600, -600, 50)
+    glVertex3f(600, -600, 100)
 
     glColor3f(1, 0.5, 1)
-    glVertex3f(600, -600, 50)
+    glVertex3f(600, -600, 100)
     glVertex3f(600, -600, 0)    
     glVertex3f(-600, -600, 0)
-    glVertex3f(-600, -600, 50)
+    glVertex3f(-600, -600, 100)
     glEnd()
+
+    glPushMatrix()
+    glColor3f(1, 0, 0)
+    glTranslatef(player_x, player_y, player_z)  
+    gluCylinder(gluNewQuadric(), 10, 15, 20, 10, 10)
+    glTranslatef(player_x+40, player_y, player_z)  
+    gluCylinder(gluNewQuadric(), 10, 15, 20, 10, 10)
+    glColor3f(0, 1, 0)
+    glTranslatef(player_x - 20, player_y, player_z+60)
+    glScalef(1.2, 0.5, 1.3)
+    glutSolidCube(60)
+    glColor3f(0, 0, 1)
+    glTranslatef(player_x , player_y, player_z+50) 
+    glutSolidSphere(20, 10, 10)
+    glColor3f(0.5, 0.5, 0.5)
+    # glRotatef(90, 0, 1, 0) 
+    glTranslatef(player_x, player_y, player_z)
+    gluCylinder(gluNewQuadric(), 15, 5, 40, 10, 10)
+    glPopMatrix()
 
 def draw_tile(is_white=True):
     """Draw a single square tile on XY plane"""
